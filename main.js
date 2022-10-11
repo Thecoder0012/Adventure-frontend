a6223f441b0fa083813339ecbf3bc01e86a2f6b3
VictorSchweitz <victorschweitz@hotmail.com>
2022-10-11 12:19:33 +0200

    Newest commit

--- main.js
+++ main.js
@@ -14,10 +14,8 @@ const isAutoSliding = true
 const autoSlideIntervalTime = 3000
 let slideInterval
 
-// document.body.classList.remove('body-overlay')
 
 // Modals
-<<<<<<< HEAD
 const openGokartModalBtn = document.getElementById('open-gokart-modal-btn')
 const openMinigolfModalBtn = document.getElementById('open-minigolf-modal-btn')
 const openPaintballModalBtn = document.getElementById('open-paintball-modal-btn')
@@ -31,21 +29,10 @@ const miniGolfModal = document.getElementById('minigolf-modal')
 const paintBallModal = document.getElementById('paintball-modal')
 const sumoWrestlingModal = document.getElementById('sumowrestling-modal')
 
-
-=======
 const openModalBtn = document.getElementsByClassName('open-modal-btn')
 const closeModalBtn = document.getElementById('close-modal-btn')
 const modal = document.querySelector('.modal')
 
-
-closeModalBtn.addEventListener('click', (e) => closeModal(e))
->>>>>>> b903f122cf0fa77d5dafc3fd88ae5a27683d72ef
-
-
-// Modal pops up when clicking the activities
-for (let i = 0; i < openModalBtn.length; i++) {
-   openModalBtn[i].addEventListener('click', openModal)
-}
 
 // EVENT LISTENERS //
 
@@ -61,22 +48,28 @@ closeSumoWrestlingModalBtn.addEventListener('click', closeSumoWrestlingModal)
 
 
 // Window Events //
-window.addEventListener('scroll', () => {
+window.addEventListener('scroll', () =>
+{
    // Adding and removing the 'shrink' class based on the scroll position on the y-axis
-   if (window.scrollY < 600) {
+   if (window.scrollY < 600)
+   {
       navBar.classList.remove('shrink')
-   } else {
+   } else
+   {
       navBar.classList.add('shrink')
    }
 
-   if (window.scrollY > 600) {
+   if (window.scrollY > 600)
+   {
       scrollToTopBtn.classList.add('active')
-   } else {
+   } else
+   {
       scrollToTopBtn.classList.remove('active')
    }
 })
 
-window.addEventListener('load', () => {
+window.addEventListener('load', () =>
+{
    navBarLinks[0].classList.add('active')
    scrollToTopOfWindow()
 })
@@ -88,18 +81,18 @@ scrollToTopBtn.addEventListener('click', scrollToTopOfWindow)
 // FUNCTIONS //
 
 // Scroll To Top of Window
-function scrollToTopOfWindow() {
+function scrollToTopOfWindow()
+{
    navBarLinks.forEach((navBarLink) => navBarLink.classList.remove('active'))
    navBarLinks[0].classList.add('active')
-   window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-   })
+   window.scrollTo({ top: 0, behavior: 'smooth' })
 }
 
 // Update active link
-navBarLinks.forEach((navBarLink) => {
-   navBarLink.addEventListener('click', () => {
+navBarLinks.forEach((navBarLink) =>
+{
+   navBarLink.addEventListener('click', () =>
+   {
       navBarLinks.forEach((navBarLink) => navBarLink.classList.remove('active'))
       navBarLink.classList.add('active')
    })
@@ -107,16 +100,19 @@ navBarLinks.forEach((navBarLink) => {
 
 // Slider //
 // Next Slide
-const nextSlide = () => {
+const nextSlide = () =>
+{
    // Get current class
    const current = document.querySelector('.current')
    // Remove current class
    current.classList.remove('current')
    // Check for next slide
-   if (current.nextElementSibling) {
+   if (current.nextElementSibling)
+   {
       // Add current to next sibling
       current.nextElementSibling.classList.add('current')
-   } else {
+   } else
+   {
       // Add current to start
       slides[0].classList.add('current')
    }
@@ -125,7 +121,8 @@ const nextSlide = () => {
 }
 
 // Previous Slide
-const previousSlide = () => {
+const previousSlide = () =>
+{
    // Get current class
    const current = document.querySelector('.current')
 
@@ -133,10 +130,12 @@ const previousSlide = () => {
    current.classList.remove('current')
 
    // Check for prev slide
-   if (current.previousElementSibling) {
+   if (current.previousElementSibling)
+   {
       // Add current to prev sibling
       current.previousElementSibling.classList.add('current')
-   } else {
+   } else
+   {
       // Add current to last
       slides[slides.length - 1].classList.add('current')
    }
@@ -145,30 +144,28 @@ const previousSlide = () => {
 }
 
 // Auto Slide
-if (isAutoSliding) {
+if (isAutoSliding)
+{
    slideInterval = setInterval(nextSlide, autoSlideIntervalTime)
 }
 
 
 // Open Modal
-<<<<<<< HEAD
 function openModal()
 {
 
 
-   if (goKartModal.classList.contains('active') || miniGolfModal.classList.contains('active') || paintBallModal.classList.contains('active') || sumoWrestlingModal.classList.contains('active'))
-   {
-=======
-function openModal() {
+
    modal.classList.add('active')
 
-   if (modal.classList.contains('active')) {
->>>>>>> b903f122cf0fa77d5dafc3fd88ae5a27683d72ef
+   if (modal.classList.contains('active'))
+   {
       document.body.classList.add('overlay')
-      overlay.classList.remove('hidden');
       document.body.style.pointerEvents = 'none'
    }
 }
+
+
 
 // Open Gokart Modal
 function openGokartModal()
@@ -200,26 +197,23 @@ function openSumoWrestlingModal()
 
 
 // Close Modal
-<<<<<<< HEAD
-function closeModal(e)
+function closeModal()
 {
 
    if (!goKartModal.classList.contains('active') || !miniGolfModal.classList.contains('active') || !paintBallModal.classList.contains('active') || !sumoWrestlingModal.classList.contains('active'))
    {
-=======
-function closeModal(e) {
-   modal.classList.remove('active')
 
-   if (!modal.classList.contains('active')) {
->>>>>>> b903f122cf0fa77d5dafc3fd88ae5a27683d72ef
-      document.body.classList.remove('overlay')
-      document.body.classList.remove('body-overlay')
-      overlay.classList.add('hidden');
-      document.body.style.pointerEvents = 'all'
+      modal.classList.remove('active')
+
+      if (!modal.classList.contains('active'))
+      {
+         document.body.classList.remove('overlay')
+         document.body.style.pointerEvents = 'all'
+      }
    }
 }
 
-<<<<<<< HEAD
+
 // Close modals
 
 // Close Gokart Modal
@@ -249,33 +243,3 @@ function closeSumoWrestlingModal()
    sumoWrestlingModal.classList.remove('active')
    closeModal()
 }
-
-=======
-
-const formValidation = () => {
-   let name = document.forms["contact"]["fullname"].value;
-   let email = document.forms["contact"]["email"].value;
-   let message = document.forms["contact"]["message"].value;
-
-   if (name === "" && email === "" && message === "") {
-      alert("Fill in the blanks!");
-   } else if (name !== "" && email.includes("@gmail.com") && message !== "") {
-      alert("Your message is sent to us! You will receive a response as soon as possible!")
-   } else if (name === "" && email === "" && message !== "") {
-      alert("You need to type in your fullname and email")
-   } else if (name === "" && email !== "" && message !== "") {
-      alert("Type in your fullname")
-   } else if (name !== "" && email === "" && message !== "") {
-      alert("Type in your mail")
-   } else if (name !== "" && email !== "" && message === "") {
-      alert("Type in your message!")
-   }
-}
-
-const sendMessage = () => {
-   document.querySelector('input[type="submit"]').addEventListener("click", formValidation)
-}
-
-
-sendMessage();
->>>>>>> b903f122cf0fa77d5dafc3fd88ae5a27683d72ef
